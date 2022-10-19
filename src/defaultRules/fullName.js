3d7f9b7d9fe2c767a82b8b5726a3bee656c07f5e
brunowacemberg <brunowacemberg@gmail.com>
2022-10-19 00:01:21 -0300

    Adjust

--- src/defaultRules/fullName.js
+++ src/defaultRules/fullName.js
@@ -8,7 +8,7 @@ export default {
     async: false,
     fn: (value) => {
         var fullNameRegex = /^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/
-        return fullNameRegex.test(value) || !value.length
+        return fullNameRegex.test(value)
     }
 
 }
